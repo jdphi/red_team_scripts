bdde51db5a90c2bfe05ab9a505719d39a082a06b
Jeremy Phipps <jeremyphipps@Jeremys-Laptop.ashcreek.asuscomm.com>
2024-04-08 23:29:07 -0600

    basic nmap scan on test host

---
+++ nmap-python/nmap1.py
@@ -0,0 +1,22 @@
+import nmap  # Import the nmap module
+
+# Create a new Nmap Scanner instance
+nm = nmap.PortScanner()
+
+# Define the target for the scan
+target = 'scanme.nmap.org'
+
+# Perform an Nmap scan on the target
+nm.scan(hosts=target, arguments='-p 22,80,443')  # Scanning for ports 22, 80, and 443
+
+# Get the scan results
+scan_results = nm.csv()  # Get the results in CSV format
+
+# Specify the filename to write the results to
+filename = 'nmap_scan_results.csv'
+
+# Write the scan results to a file
+with open(filename, 'w') as file:
+    file.write(scan_results)
+
+print(f'Scan results have been saved to {filename}')
